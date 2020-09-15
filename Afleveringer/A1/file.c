4461383773f880e99683b10240e9b99ac3d8d2d8
Daniel Stephensen <daniel@stephensen.net>
2020-09-15 14:59:45 +0200

    exercise 1.1

--- Afleveringer/A1/file.c
+++ Afleveringer/A1/file.c
@@ -48,26 +48,32 @@ int check_type(FILE *file) {
 int main(int argc, char *argv[]) {
   //check number of arguments
   //if no arguments, print to stderr
-  if(argc == 1) {
-	  fprintf(stderr, "Usage: file path");
-	  return EXIT_FAILURE;
-  //if number of arguments is not exactly 1, fail
-  } else if(argc!=2) {
-	  return EXIT_FAILURE;
-  //if 1 argument, proceed with the given path
-  } else {
-	  path = argv[1];
-  }
+	if(argc == 1) {
+		fprintf(stderr, "Usage: file path");
+		return EXIT_FAILURE;
+  	}
+  	else 
+  	{
+  		int i;
+		//The longest path type is ascii, which is in index 2 of the FILE_TYPE_STRINGS array
+		int max_length = strlen(FILE_TYPE_STRINGS[2]);
+  		for (i = 1; i < argc; i++)
+		{
+	  		path = argv[i];
+	  		if(access(path, F_OK) == 0) {
+				file = fopen(path, "r");
+				fprintf(stdout, "%s:%*s %s\n", path, max_length - strlen(path),FILE_TYPE_STRINGS[check_type(file)]);
+  	  		} else {
+	    		return print_error(path, errno);
+	  		}
+		}
+		return EXIT_SUCCESS;
+  	}
+  
 
   //check if a file exists on path, if it does, open it and print typecheck string to stdout,
   //otherwise display error message
-  if(access(path, F_OK) == 0) {
-	  file = fopen(path, "r");
-	  fprintf(stdout, "%s: %s\n", path, FILE_TYPE_STRINGS[check_type(file)]);
-    return EXIT_SUCCESS;
-  } else {
-	  return print_error(path, errno);
-  }
+  
 }
 
 
