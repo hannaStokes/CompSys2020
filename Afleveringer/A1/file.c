3ae5afbdc459379a764593569d9ad6a1bfa378d9
Jesper <JespHass@gmail.com>
2020-09-19 20:39:56 +0200

    fixed merge errors, added tons of comments and fixed minor syntax errors in almost done implementation

--- Afleveringer/A1/file.c
+++ Afleveringer/A1/file.c
@@ -14,17 +14,10 @@ enum file_type {
 	DATA,
 	EMPTY,
 	ASCII,
-<<<<<<< HEAD
-	ISO8859;
-	UTF8;
-	LITTLE_ENDIAN;
-	BIG_ENDIAN;
-=======
 	ISO8859,
 	UTF8,
-	LittleEdian,
-	BigEdian
->>>>>>> 335c4b63a0e2e7d50b3131de75044236d5ec13b9
+	LITTLE_ENDIAN,
+	BIG_ENDIAN
 };
 
 const char * const FILE_TYPE_STRINGS[] = {
@@ -33,8 +26,8 @@ const char * const FILE_TYPE_STRINGS[] = {
 	"ASCII text",
 	"ISO-8859",
 	"UTF-8 Unicode Text",
-	"Little-edian UTF-16 Unicode Text",
-	"Big-edian UTF-16 Unicode Text"
+	"Little-endian UTF-16 Unicode Text",
+	"Big-endian UTF-16 Unicode Text"
 };
 
 enum ASCII_Constraints {
@@ -68,201 +61,109 @@ int check_type(FILE *file) {
 	int notUTF8 = 0;
 
 	//if the first char encountered is the end of file marker
-	if (c == EOF) {
+	if (currentChar == EOF) {
 		return EMPTY;
-<<<<<<< HEAD
 	}
+	//Unless the file ends after the first byte of data...
+	if (nextChar != EOF) {
 
-	//iterate through the file, if you encounter something that is not in the 
-	//accepted ranges of ASCII characters, deem the file data, otherwise ASCII
-	while (currentChar != EOF && !(notASCII && notISO && notUTF8)) {
-		
-		//Check if the file is UTF-16
-		if (nextChar != EOF) {
-
-			//Check if the file is little-endian UTF-16
-			if ((currentChar == 0xFF) && (nextChar == 0xFE)) {
-				notASCII = 1;
-=======
-	} 
-	int notAscii = 0;
-	int notISO = 0;
-	int notUTF8 = 0;
-	/*int notLittleEdian = 0;*/
-	/*int notBigEdian = 0;*/
+		//...check if the file is little-endian UTF-16
+		if (((unsigned char)currentChar == 0xFF) && ((unsigned char)nextChar == 0xFE)) {
+			return LITTLE_ENDIAN;
+	        } else if (((unsigned char)currentChar == 0xFE) && ((unsigned char)nextChar == 0xFF)) {
+			return BIG_ENDIAN;
+		}
+	}
 
 	int ekstraByte = 0;
 
-	//iterate through the file, if you encounter something that is not in the 
-	//accepted ranges of ASCII characters, deem the file data, otherwise ASCII
-	while(c != EOF) {
-		if (!(((bell <= c) && (c <= carriageReturn))
-				|| (c == escape)
-				|| ((space <= c) && (c <= equivalencySign)))) {
-			notAscii = 1;
-			if (!((nonBreakingSpace <= (unsigned char) c) && ((unsigned char) c <= yWithDiaeresis))){
->>>>>>> 335c4b63a0e2e7d50b3131de75044236d5ec13b9
-				notISO = 1;
-				notUTF8 = 1;
-				return LITTLE_ENDIAN;
-
-			//Check if the file is big-endian UTF-16
-			} else if ((currentChar == 0xFE) && (nextChar == 0xFF)) {
-				notASCII = 1;
-				notISO = 1;
-				notUTF8 = 1;
-				return BIG_ENDIAN;
-
-			} else {
-				file--;
-			}
-		}
-
-		//Have all bytes been ASCII so far?
-		if ( !(notASCII) )  {
-			//Is the current byte not a ASCII?
-			if ( !(((bell <= currentChar) && (currentChar <= carriageReturn))
+	//Check file byte for byte
+	while(currentChar != EOF) {
+		//Check for symbols not in ASCII intervals provided in assignment text
+		if (!(((bell <= currentChar) && (currentChar <= carriageReturn))
 				|| (currentChar == escape)
 				|| ((space <= currentChar) && (currentChar <= equivalencySign)))) {
-				notASCII = 1;
-			}
-		} 
-<<<<<<< HEAD
-
-		//Have any bytes not been ASCII and have all others been ISO-8859?
-		if ( !(notISO) && notASCII ) {
-			//Is the current byte not a ISO-8859?
-			if ( !((nonBreakingSpace <= currentChar) && (currentChar <= yWithDiaeresis))) {
-				notISO = 1;
-			}
+			//If a conflict is found, the file is not ASCII
+			notASCII = 1;
 		}
 
-		//Have any bytes not been ISO-8859 and have all others been UTF8?
-		if (notISO && !(notUTF8)) {
-			//Is the current byte not a UTF-8?
-			if (0b11110000 = (currentChar & (0b11110000))) {
-
-				char followingChar = fgetc(file);
-				for (int i = 0; i < 2; i++) {
-					if ( (followingChar == EOF) || ((0b10000000) != (followingChar & (0b10000000)))) {
-						notUTF8 = 1;
-					}
-					char get = fgetc(file);
-				}
-
-			} else if (0b11100000 != (currentChar & 0b11100000)) {
-				
-				char followingChar = fgetc(file);
-				for (int i = 0; i < 1; i++) {
-					if ( (followingChar == EOF) || ((0b10000000) != (followingChar & (0b10000000)))) {
-						notUTF8 = 1;
-					}
-					char followingChar = fgetc(file);
-				}
-
-			} else if ((0b110000 != (currentChar & 0b110000))) {
-
-				char followingChar = fgetc(file);
-				if ( (followingChar == EOF) && ((0b10000000) != (followingChar & (0b10000000)))) { 
-					notUTF8 = 1;
-				}
-
-			} else {
-				notUTF8 = 1;
-			}
+		//Check file for symbols breaking the ISO-8859 standard (endpoint omitted, as currentChar <= yWithDiaresis will always be true for a char)
+		if (!((nonBreakingSpace <= (unsigned char) currentChar))){
+			//If a conflict is found, the file is not ISO-8859
+			notISO = 1;
 		}
 
-=======
-		if (UTF8_Extra(c)) {
+		//Start of UTF-8 check
+                //Check if the byte we are looking at is the UTF-8 data byte in multi-byte represented UTF-8 symbols
+		if (UTF8_Extra(currentChar)) {
 			if (ekstraByte == 0) {
 				notUTF8 = 1;
-			}
-			else {
+			} else {
 				ekstraByte--;
 			}
-		}
-		else if (UTF8_1B(c)) {
+		//Check if the byte we are looking at is the UTF-8 marker for 1-byte UTF-8 symbols
+		} else if (UTF8_1B(currentChar)) {
 			if (ekstraByte != 0) {
 				notUTF8 = 1;
 			}
-		}
-		else if (UTF8_2B(c)) {
+		//Check if the byte we are looking at is the UTF-8 marker for 2-byte UTF-8 symbols
+		} else if (UTF8_2B(currentChar)) {
 			if (ekstraByte != 0) {
 				notUTF8 = 1;
-			}
-			else {
+			} else {
 				ekstraByte++;
 			}
-		}
-		else if (UTF8_3B(c)) {
+		//Check if the byte we are looking at is the UTF-8 marker for 3-byte UTF-8 symbols
+		} else if (UTF8_3B(currentChar)) {
 			if (ekstraByte != 0) {
 				notUTF8 = 1;
-			}
-			else {
+			} else {
 				ekstraByte += 2;
 			}
-		}
-		else if (UTF8_4B(c)) {
+		//Check if the byte we are looking at is the UTF-8 marker for 4-byte UTF-8 symbols
+		} else if (UTF8_4B(currentChar)) {
 			if (ekstraByte != 0) {
 				notUTF8 = 1;
-			}
-			else {
+			} else {
 				ekstraByte += 3;
 			}
-		}
-		else {
+		//If all the above fails, then the file is not UTF-8
+		} else {
 			notUTF8 = 1;
 		}
 		
->>>>>>> 335c4b63a0e2e7d50b3131de75044236d5ec13b9
-		c = fgetc(file);
-
+		//Check next byte
+		currentChar = nextChar;
+		nextChar = fgetc(file);
 	}
 
 	fclose(file);
 
-<<<<<<< HEAD
 	// Determine which file type
-	if (notASCII && notISO && notUTF8) {
-		return DATA;
-	} else if (notASCII && notISO) {
-		return UTF8;
-	} else if (notASCII) {
+	if (!notASCII) {
+		return ASCII;
+	} else if (!notISO) {
 		return ISO8859;
+	} else if (!notUTF8) {
+		return UTF8;
 	} else {
-		return ASCII;
-	}
-
-=======
-	if (!notAscii) {
-		return ASCII;
-	}
-	else if (!notISO) {
-		return ISO8859;
-	}
-	else if (!notUTF8) {
-		return UTF8;
-	}
-	else {
 		return DATA;
 	}
->>>>>>> 335c4b63a0e2e7d50b3131de75044236d5ec13b9
 }
 
-int Max_Length(int str_length, char *str[])
-{
-    int longest = 0;
+int Max_Length(int str_length, char *str[]) {
+	int longest = 0;
 	
 	//Starts from i=1 because the first argument of argv should not be included
-    for(int i = 1; i < str_length; i++)
-    {
-        int len = strlen(str[i]);
-        if(longest < len)
-        {
-           longest = len;
-        }
-   }
-   return longest;
+    	for(int i = 1; i < str_length; i++)
+    	{
+        	int len = strlen(str[i]);
+        	if(longest < len)
+        	{
+           		longest = len;
+        	}
+   	}
+   	return longest;
 }
 
 int main(int argc, char *argv[]) {
@@ -271,14 +172,11 @@ int main(int argc, char *argv[]) {
 	if(argc == 1) {
 		fprintf(stderr, "Usage: file path/n");
 		return EXIT_FAILURE;
-  	}
-  	else 
-  	{
+  	} else {
   		int i;
 		//The longest path type is ascii, which is in index 2 of the FILE_TYPE_STRINGS array
 		int max_length = Max_Length(argc, argv);
-  		for (i = 1; i < argc; i++)
-		{
+  		for (i = 1; i < argc; i++) {
 	  		path = argv[i];
 	  		if(access(path, R_OK) == 0) {
 				file = fopen(path, "r");
